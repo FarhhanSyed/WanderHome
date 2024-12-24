92f0e21710fd1803d81b99464e4b3e3767a2fac5
FarhhanSyed <farhansyed2045@gmail.com>
2024-12-24 22:39:10 +0530

    new and post route

--- app.js
+++ app.js
@@ -32,6 +32,18 @@ app.get("/listings",async (req,res)=>{
     res.render("./listings/index.ejs",{allListing});
 })
 
+//New Route
+app.get("/listings/new",(req,res)=>{
+    res.render("./listings/new.ejs");
+})
+
+//Post Route
+app.post("/listings",async (req,res)=>{
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("./listings/");
+})
+
 //Show Route
 app.get("/listings/:id",async (req,res)=>{
     let {id}=req.params;    
