bd5a524f3d4d8f11fa64eb43f637c9e88f837b29
FarhhanSyed <farhansyed2045@gmail.com>
2025-03-22 16:14:00 +0530

    final changes

--- app.js
+++ app.js
@@ -84,18 +84,9 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user;
+  res.locals.currUser = req.user || null;
   next();
 });
-
-// app.get("/demo",async (req,res)=>{
-//     let fakeUser=new User({
-//         email:"syed@123",
-//         username:"rocky",
-//     });
-//     let registeresUser=await User.register(fakeUser,"hello");
-//     res.send(registeresUser);
-// })
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
